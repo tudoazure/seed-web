1dba33e508267b77c6830a010f6389edebbb7008
Sonu Kumar <contact@tudostack.com>
2014-09-24 23:45:01 +0530

    fixes

--- public/libs/PaytmIM/config/PaytmIM.Config.js
+++ public/libs/PaytmIM/config/PaytmIM.Config.js
@@ -12,8 +12,28 @@ var Globals = {
     StropheConnect : chatHostBaseURI  + "/http-bind/",
 
     // Following should be replaced in the implementation
-    AgentId : "2kxq45",
-    UserLogin : "webbargain@paytm.com",
-    UserToken : "aceb388d-87f2-4781-9f3b-b36c8d1d4e79"
+    AgentId : "piayrq",
+    UserLogin : "paytmuser7@mailinator.com",
+    UserToken : "d7fa7907-e6c9-4b0c-a830-6fe2f86efa5d"
  }      
 })();
+
+
+// For Testing Purpose at staging
+// Agents:
+// 1) paytmagent1  : piayrq
+// 2) paytmagent2  : 2kxq45
+// 3) paytmagent3  : m0fwaq
+// 4) paytmagent4  : qc94z6
+// 5) paytmagent5  : fmpwrn
+
+
+// Users:
+// 1) login : "webbargain@paytm.com",
+//   accessToken : "aceb388d-87f2-4781-9f3b-b36c8d1d4e79"
+// 2) login : "paytmuser1@mailinator.com",
+//   accessToken : "3de7a0ff-13e1-4421-b3a2-043f708a5e9e"
+// 3) login : "paytmuser2@mailinator.com",
+//   accessToken : "2f801d52-660e-4e13-8eb2-2c5421966807"
+// 4) login : "paytmuser7@mailinator.com"
+//   accessToken : "d7fa7907-e6c9-4b0c-a830-6fe2f86efa5d"
